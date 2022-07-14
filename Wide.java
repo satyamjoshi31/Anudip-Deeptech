f853b9604a9849383b140cc654d08f57c423513a
satyamjoshi31 <106659288+satyamjoshi31@users.noreply.github.com>
2022-07-14 16:40:49 +0530

    Add files via upload

---
+++ Wide.java
@@ -0,0 +1,17 @@
+class Wide
+
+{
+	public static void main(String args[])
+	{
+		int myi = 83;
+		double myd = myi;
+		System.out.println(" Given integer is " + myi);
+		System.out.println(" Double value of integer is " + myd);
+
+
+	}
+
+
+
+
+}
