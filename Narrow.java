fb6e446f98b4d44a6a19545a9ac79cdd327901d6
satyamjoshi31 <106659288+satyamjoshi31@users.noreply.github.com>
2022-07-14 16:25:03 +0530

    Add files via upload

---
+++ Narrow.java
@@ -0,0 +1,24 @@
+
+
+
+class Narrow
+
+{
+
+	public static void main(String args[])
+	{
+		// double to int
+		double myd = 10.46;
+		int myi = (int) myd;
+		System.out.println(myd);
+		System.out.println(myi);
+
+
+		double myd2 = 7.89;
+		int myi2 = (int) myd2;
+		System.out.println(myd2);
+		System.out.println(myi2);
+	}
+
+
+}
